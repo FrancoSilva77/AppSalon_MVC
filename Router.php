8efc5c41452d1fc6e3e0118ed48fc6da8d6d206b
Franco Silva <franco_tigres18@hotmail.com>
2024-01-03 09:40:55 -0600

    Correcion en Router.php

--- Router.php
+++ Router.php
@@ -20,12 +20,8 @@ class Router
     public function comprobarRutas()
     {
         session_start();
-        if ($_SERVER['PATH_INFO']) {
-            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-        } else {
-            $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
-        }
-
+        
+        $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
