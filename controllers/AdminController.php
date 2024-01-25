40118ba476610b2ec850159d5a4bd803c3f2f5b2
Franco Silva <franco_tigres18@hotmail.com>
2024-01-25 15:14:07 -0600

    Consulta update

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -23,13 +23,15 @@ class AdminController
         }
 
         // Consultar la base de datos
-        $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
+        $consulta = "SELECT citas.id, horas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
         $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
         $consulta .= " FROM citas  ";
         $consulta .= " LEFT OUTER JOIN usuarios ";
         $consulta .= " ON citas.usuarioId=usuarios.id  ";
         $consulta .= " LEFT OUTER JOIN citasServicios ";
         $consulta .= " ON citasServicios.citaId=citas.id ";
+        $consulta .= " LEFT OUTER JOIN horas ";
+        $consulta .= " ON citas.horaId = horas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
         $consulta .= " ON servicios.id=citasServicios.servicioId ";
         $consulta .= " WHERE fecha =  '${fecha}' ";
